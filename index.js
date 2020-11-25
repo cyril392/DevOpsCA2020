a1835bb7309a0ceb586afbd4631e69b508601b3b
Cyril <cyril392@hotmail.fr>
2020-11-25 14:20:17 +0100

    debug index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-console.log("Patate 1");
+console.log("Patate: 1");
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -10,5 +10,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
-    console.log("Patate 2");
+    console.log("Patate: 2");
 })
