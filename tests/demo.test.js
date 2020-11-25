1d6367f18809600b1740bb2a38e6c43b6c6a3021
Cyril <cyril392@hotmail.fr>
2020-11-25 12:06:31 +0100

    failing test

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -7,6 +7,6 @@ describe("Checking if the user is created correctly", function(){
     it("shoyld create the user with the correct name", function(){
         debugger
         var tom = new User("tom");
-        tom.name.should.be.equal("tom");
+        tom.name.should.be.equal("tommy");
     })
 })
