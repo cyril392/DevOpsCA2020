622610366b563470caf22819aa73c0cc00dd1912
Cyril <cyril392@hotmail.fr>
2020-11-25 13:42:12 +0100

    init clever

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git remote add cleverapps
-https://$CLEVER_TOKEN:$CLEVER_SECRET@push.clever-cloud.com/app_YOUR_APPLICATION_ID.git
+https://$CLEVER_TOKEN:$CLEVER_SECRET@push.clever-cloud.com/app_5c5194bd-3152-4fb5-8474-8856373e1281.git
 git --verbose --force push cleverapps master
